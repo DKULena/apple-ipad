a1b09f02874af704d5951cebc1f930aad51f5ac2
DKULena <32173057@dankook.ac.kr>
2025-02-22 18:07:53 +0900

    add: control search(js)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 장바구니
 const $basketStarter = document.querySelector('header .basket-starter');
 const $basket = $basketStarter.querySelector('.basket');
 
@@ -21,3 +22,25 @@ $basket.addEventListener("click", (event) => {
 window.addEventListener("click", () => {
     hideBasket();
 });
+
+// 검색
+const $header = document.querySelector('header');
+const $searchWrap = $header.querySelector('.search-wrap');
+const $searchStarter = $header.querySelector('.search-starter');
+const $searchCloser = $searchWrap.querySelector('.search-closer');
+const $shadow = $searchWrap.querySelector('.shadow');
+
+const showSearch = () => {
+    $header.classList.add('searching');
+    document.documentElement.classList.add('fixed');
+}
+
+const hideSearch = () => {
+    $header.classList.remove('searching');
+    document.documentElement.classList.remove('fixed');
+}
+
+$searchStarter.addEventListener("click", showSearch);
+
+$searchCloser.addEventListener("click", hideSearch);
+$shadow.addEventListener("click", hideSearch);
