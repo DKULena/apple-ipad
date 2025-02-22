f20aaadf31250f96c4e40b7d2be20889557a64ef
DKULena <32173057@dankook.ac.kr>
2025-02-22 19:26:40 +0900

    add: search bar animation(js)

--- js/main.js
+++ js/main.js
@@ -1,44 +1,60 @@
 // 장바구니
-const $basketStarter = document.querySelector('header .basket-starter');
-const $basket = $basketStarter.querySelector('.basket');
+const $basketStarter = document.querySelector("header .basket-starter");
+const $basket = $basketStarter.querySelector(".basket");
 
 const showBasket = () => {
-    $basket.classList.add('show');
-}
+  $basket.classList.add("show");
+};
 
 const hideBasket = () => {
-    $basket.classList.remove('show');
-}
+  $basket.classList.remove("show");
+};
 
 $basketStarter.addEventListener("click", (event) => {
-    event.stopPropagation();
-    $basket.classList.contains('show') ? hideBasket() : showBasket()
-})
+  event.stopPropagation();
+  $basket.classList.contains("show") ? hideBasket() : showBasket();
+});
 
 $basket.addEventListener("click", (event) => {
-    event.stopPropagation();
-})
+  event.stopPropagation();
+});
 
 window.addEventListener("click", () => {
-    hideBasket();
+  hideBasket();
 });
 
 // 검색
-const $header = document.querySelector('header');
-const $searchWrap = $header.querySelector('.search-wrap');
-const $searchStarter = $header.querySelector('.search-starter');
-const $searchCloser = $searchWrap.querySelector('.search-closer');
-const $shadow = $searchWrap.querySelector('.shadow');
+const $header = document.querySelector("header");
+const $headerMenuList = [...$header.querySelectorAll("ul.menu > li")];
+const $searchWrap = $header.querySelector(".search-wrap");
+const $searchStarter = $header.querySelector(".search-starter");
+const $searchCloser = $searchWrap.querySelector(".search-closer");
+const $shadow = $searchWrap.querySelector(".shadow");
+const $searchDelay = [...$searchWrap.querySelectorAll("li")];
 
 const showSearch = () => {
-    $header.classList.add('searching');
-    document.documentElement.classList.add('fixed');
-}
+  $header.classList.add("searching");
+  document.documentElement.classList.add("fixed");
+  $headerMenuList.reverse().forEach(($li, index) => {
+    $li.style.transitionDelay = (index * 0.4) / $headerMenuList.length + "s";
+  });
+
+  $searchDelay.forEach(($li, index) => {
+    $li.style.transitionDelay = (index * 0.4) / $searchDelay.length + "s";
+  });
+};
 
 const hideSearch = () => {
-    $header.classList.remove('searching');
-    document.documentElement.classList.remove('fixed');
-}
+  $header.classList.remove("searching");
+  document.documentElement.classList.remove("fixed");
+  $headerMenuList.reverse().forEach(($li, index) => {
+    $li.style.transitionDelay = (index * 0.4) / $headerMenuList.length + "s";
+  });
+  $searchDelay.reverse().forEach(($li, index) => {
+    $li.style.transitionDelay = (index * 0.4) / $searchDelay.length + "s";
+  });
+  $searchDelay.reverse();
+};
 
 $searchStarter.addEventListener("click", showSearch);
 
